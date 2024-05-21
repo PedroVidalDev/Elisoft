59d7acac7c6fe84ace8ea41f1852dc7fcc03a998
PedroVidalDev <pedrohvidals@gmail.com>
2024-05-21 12:35:49 -0300

    feat: adicionando protecao nas rotas de produto e venda

--- src/routes/ProdutoRoutes.js
+++ src/routes/ProdutoRoutes.js
@@ -1,13 +1,14 @@
 const { Router } = require("express");
 const ProdutoController = require("../controllers/ProdutoController.js");
+const autenticarToken = require("../middlewares/autenticacao.js");
 
 const router = Router();
 
 const produtoController = new ProdutoController();
 
-router.get("/produtos", (req, res) => produtoController.pegaTodos(req, res));
-router.get("/produtos/:id", (req, res) => produtoController.pegaUmPorId(req, res))
-router.post("/produtos", (req, res) => produtoController.criarProduto(req, res));
-router.delete("/produtos/:id", (req, res) => produtoController.exclui(req, res));
+router.get("/produtos", autenticarToken, (req, res) => produtoController.pegaTodos(req, res));
+router.get("/produtos/:id", autenticarToken, (req, res) => produtoController.pegaUmPorId(req, res))
+router.post("/produtos", autenticarToken, (req, res) => produtoController.criarProduto(req, res));
+router.delete("/produtos/:id", autenticarToken, (req, res) => produtoController.exclui(req, res));
 
 module.exports = router;
--- src/routes/VendaRoutes.js
+++ src/routes/VendaRoutes.js
@@ -1,13 +1,14 @@
 const { Router } = require("express");
 const VendaController = require("../controllers/VendaController.js");
+const autenticarToken = require("../middlewares/autenticacao.js");
 
 const router = Router();
 
 const vendaController = new VendaController();
 
-router.get("/vendas", (req, res) => vendaController.pegaTodos(req, res));
-router.get("/vendas/:id", (req, res) => vendaController.pegaUmPorId(req, res))
-router.post("/vendas", (req, res) => vendaController.criarVenda(req, res));
-router.delete("/vendas/:id", (req, res) => vendaController.exclui(req, res));
+router.get("/vendas", autenticarToken, (req, res) => vendaController.pegaTodos(req, res));
+router.get("/vendas/:id", autenticarToken, (req, res) => vendaController.pegaUmPorId(req, res))
+router.post("/vendas", autenticarToken, (req, res) => vendaController.criarVenda(req, res));
+router.delete("/vendas/:id", autenticarToken, (req, res) => vendaController.exclui(req, res));
 
 module.exports = router;
