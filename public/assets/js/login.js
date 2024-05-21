4775734c285848550842561e0ee74ed11d902e17
PedroVidalDev <pedrohvidals@gmail.com>
2024-05-21 15:52:50 -0300

    feat: adicionando script para fazer req http no login e adicionando arquivo js como module no html

---
+++ public/assets/js/login.js
@@ -0,0 +1,17 @@
+import request from "./utils/requestHttp.js";
+import { header } from "./utils/header.js";
+
+const form = document.querySelector("#form");
+
+form.addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    let formData = new FormData(form);
+    let dados = Object.fromEntries(formData);
+    let jsonDados = JSON.stringify(dados);
+
+    const reqData = await request("login", "POST", header, jsonDados);
+
+    localStorage.setItem("token", reqData.token);
+    console.log(reqData);
+})
