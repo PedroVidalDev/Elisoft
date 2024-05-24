c911f108b0cb586f44d85fc97b27bd48ec85858b
PedroVidalDev <pedrohvidals@gmail.com>
2024-05-24 15:49:59 -0300

    feat: criando algoritmo para mostrar produtos na tela

--- public/assets/js/produto/produto.js
+++ public/assets/js/produto/produto.js
@@ -1,3 +1,58 @@
-const botaoCriar = document.querySelector("#criar-botao");
+import request from "./../utils/requestHttp.js";
+import { headerAuth } from "./../utils/header.js";
 
-botaoCriar.addEventListener("click", () => { window.location.href = "/pages/produto/criarProduto.html"})
+const botaoCriar = document.querySelector("#criar-botao");
+const tbody = document.querySelector("tbody");
+
+window.onload = async () => {
+    const reqData = await request("produtos", "GET", headerAuth, null);
+
+    tbody.innerHTML = "";
+
+    reqData.forEach(produto => {
+
+        const tr = document.createElement("tr");
+
+        const tdNome = document.createElement("td");
+        tdNome.innerHTML = produto.nome;
+
+        const tdQuantidade = document.createElement("td");
+        tdQuantidade.innerHTML = produto.quantidade;
+
+        const tdPreco = document.createElement("td");
+        tdPreco.innerHTML = `R$ ${produto.preco}`;
+
+        const tdAcao = document.createElement("td")
+
+        const botaoEditar = document.createElement("button");
+        botaoEditar.className = "botao-acao";
+        botaoEditar.innerHTML = "E";
+
+        const botaoExcluir = document.createElement("button");
+        botaoExcluir.className = "botao-acao";
+        botaoExcluir.innerHTML = "X";
+
+        tdAcao.appendChild(botaoEditar);
+        tdAcao.appendChild(botaoExcluir);
+
+        const tdVender = document.createElement("td");
+
+        const botaoVender = document.createElement("button");
+        botaoVender.className = "botao-acao";
+        botaoVender.innerHTML = "V";
+
+        tdVender.appendChild(botaoVender);
+
+        tr.appendChild(tdNome);
+        tr.appendChild(tdQuantidade);
+        tr.appendChild(tdPreco);
+        tr.appendChild(tdAcao);
+        tr.appendChild(tdVender);
+
+        tbody.appendChild(tr);
+    });
+}
+
+
+
+botaoCriar.addEventListener("click", () => { window.location.href = "/pages/produto/criarProduto.html"});
