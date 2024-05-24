5ee7b9117e64885ced1c25e6d054df05f5796dfa
PedroVidalDev <pedrohvidals@gmail.com>
2024-05-24 15:35:07 -0300

    feat: separando funcao de criar produto em outro arquivo js

---
+++ public/assets/js/produto/criarProduto.js
@@ -0,0 +1,21 @@
+import { headerAuth } from "./../utils/header.js";
+import request from "./../utils/requestHttp.js"
+
+const form = document.querySelector("#form");
+
+form.addEventListener("submit", async (event) => {
+    event.preventDefault();
+
+    let formData = new FormData(form);
+    let dados = Object.fromEntries(formData);
+    let jsonDados = JSON.stringify(dados);
+
+    const reqData = await request("produtos", "POST", headerAuth, jsonDados);
+
+    if(reqData != null){
+        alert(reqData.mensagem);
+        window.location.href = "/pages/produto/produtos.html";
+    } else{
+        alert("Erro no login. Favor verificar suas informacoes.");
+    }
+})
