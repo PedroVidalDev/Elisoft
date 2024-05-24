956d5df645036181af9cb24e50076752b4709249
PedroVidalDev <pedrohvidals@gmail.com>
2024-05-23 21:45:24 -0300

    feat: criando rota para resgatar infos do fluxo de caixa

--- src/routes/VendaRoutes.js
+++ src/routes/VendaRoutes.js
@@ -8,6 +8,8 @@ const vendaController = new VendaController();
 
 router.get("/vendas", autenticarToken, (req, res) => vendaController.pegaTodos(req, res));
 router.get("/vendas/:id", autenticarToken, (req, res) => vendaController.pegaUmPorId(req, res))
+router.get("/vendas/fluxo", autenticarToken, (req, res) => vendaController.resgatarFluxoDeCaixa(req, res));
+
 router.post("/vendas", autenticarToken, (req, res) => vendaController.criarVenda(req, res));
 router.delete("/vendas/:id", autenticarToken, (req, res) => vendaController.exclui(req, res));
 
