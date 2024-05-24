f2e9a1c25a18792fa8dedbb52c5ba4a676bee958
PedroVidalDev <pedrohvidals@gmail.com>
2024-05-24 14:03:35 -0300

    feat: adicionando id do usuario na criacao do produto

--- src/service/ProdutoService.js
+++ src/service/ProdutoService.js
@@ -14,23 +14,35 @@ class ProdutoService extends Service{
 
         if(usuarioEncontrado == null){
             console.log("Usuario nao existe.")
+            return null
         }
 
         if(dadoExistente != null){
             console.log("Produto ja existe.")
+            return null
         }
 
         if(dadosDoRegistro.preco < 0){
             console.log("Preco menos que 0");
+            return null
         }
 
         if(dadosDoRegistro.quantidade < 0){
             console.log("Estoque menor que 0");
+            return null
         }
 
         else{
-            return {mensagem: "Produto criado com sucesso!", objeto: await this.criaRegistro(dadosDoRegistro)};
-        }
+            return {
+                mensagem: "Produto criado com sucesso!", 
+                objeto: await this.criaRegistro({                    
+                    nome: dadosDoRegistro.nome,
+                    descricao: dadosDoRegistro.descricao,
+                    preco: dadosDoRegistro.preco,
+                    quantidade: dadosDoRegistro.quantidade,
+                    usuario_id: usuarioEncontrado.id
+                })};
+        } 
 
     }
 }
