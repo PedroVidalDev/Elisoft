30b4b2aea9663ca3c9e4033cacc06c73e0c6f747
PedroVidalDev <pedrohvidals@gmail.com>
2024-05-26 10:37:24 -0300

    feat: criando controller para editar produto

--- src/controllers/ProdutoController.js
+++ src/controllers/ProdutoController.js
@@ -16,6 +16,23 @@ class ProdutoController extends Controller{
 
     }
 
+    async editarProduto(req, res){
+        const {id} = req.params;
+        const dadosAtualizados = req.body;
+
+        const foiAtualizado = await this.service.atualizaProduto(id, dadosAtualizados);
+
+        if(foiAtualizado){
+            return res.status(200).json({
+                mensagem: "Produto foi atualizado."
+            });
+        } else{
+            return res.status(400).json({
+                message: "Produto nao pode ser atualizado. Erro."
+            });
+        }
+    }
+
 }
 
 module.exports = ProdutoController;
